f0b6dc60a4167c4529363eee050c8863a7e4396e
Jared Fitzpatrick <jaredjfitzpatrick@gmail.com>
2018-01-23 10:56:33 -0800

    Added DevU Challenge
    Added the first challenge from "C# Fundamentals via ASP.NET Web Applications" course on DevU

---
+++ DevU C# Course Challenges/1 - ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <span class="auto-style1">First Number:</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="firstValueTextBox" runat="server"></asp:TextBox>
+            <br />
+            <span class="auto-style1">Second Number:</span>&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="secondValueTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="+" Width="26px" />
+&nbsp;<asp:Button ID="subtractButton" runat="server" OnClick="subtractButton_Click" Text="-" Width="26px" />
+&nbsp;<asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" Width="26px" />
+&nbsp;<asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" Text="/" Width="26px" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" BackColor="#6699FF" Font-Bold="True" Font-Size="X-Large"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DevU C# Course Challenges/1 - ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(firstValueTextBox.Text);
+            double secondValue = double.Parse(secondValueTextBox.Text);
+            double resultValue = firstValue + secondValue;
+            resultLabel.Text = resultValue.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(firstValueTextBox.Text);
+            double secondValue = double.Parse(secondValueTextBox.Text);
+            double resultValue = firstValue - secondValue;
+            resultLabel.Text = resultValue.ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(firstValueTextBox.Text);
+            double secondValue = double.Parse(secondValueTextBox.Text);
+            double resultValue = firstValue * secondValue;
+            resultLabel.Text = resultValue.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(firstValueTextBox.Text);
+            double secondValue = double.Parse(secondValueTextBox.Text);
+            double resultValue = firstValue / secondValue;
+            resultLabel.Text = resultValue.ToString();
+        }
+    }
+}
